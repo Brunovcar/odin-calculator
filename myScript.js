c7b2b033469b62ba096ee649ce2ea36ff4552ed4
Brunovcar <89946777+Brunovcar@users.noreply.github.com>
2024-11-05 08:17:17 -0300

    Version 1 - Odin calculator - need improvements

--- myScript.js
+++ myScript.js
@@ -1,12 +1,17 @@
-let firstNumber =  [];
-let secondNumber = [];
-let operator = [];
+let displayString = [];
+let inputString =  [];
+let inputAux = [];
+let operationNumber1;
+let operationNumber2;
+let operationOperator;
+let equalOperation = false;
+
 
 function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
 
-function substract(firstNumber, secondNumber) {
+function subtract(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
 }
 
@@ -19,28 +24,81 @@ function divide(firstNumber, secondNumber) {
 }
 
 function operate(operator, firstNumber, secondNumber) {
-    if(operator == "+") {
+    if(operator == '+') {
         return add(firstNumber, secondNumber);
     }
-    if(operator == "-") {
-        return substract(firstNumber, secondNumber);
+    if(operator == '-') {
+        return subtract(firstNumber, secondNumber);
     }
-    if(operator == "*") {
+    if(operator == '*') {
         return multiply(firstNumber, secondNumber);
     }
-    if(operator == "/") {
+    if(operator == '/') {
+        if(secondNumber == '0') {
+            return "Bruh... press CLEAR to continue";
+        }
         return divide(firstNumber, secondNumber);
     }
 }
 
-function display() {
+const showDisplay = document.querySelector('.display')
 
+function display(displayValue) {
+    showDisplay.innerHTML = displayValue;
 }
 
-const keypadSelection = document.querySelector('container');
+const keypadSelection = document.querySelector('.container');
 
-pressButton.addEventListener('click', function(e) {
+keypadSelection.addEventListener('click', function(e) {
     const buttonSelection = e.target;
 
+    if(buttonSelection.className == 'number') {
+        if(equalOperation == true) {
+            inputAux = [];
+            equalOperation = false;
+        }
+        inputAux.push(buttonSelection.textContent);
+        inputAux[0] = inputAux.join('');
+        inputAux.splice(1,1);
+        display(inputAux[0]);
+    }
+
+    if(buttonSelection.className == 'operator') {
+        if(equalOperation == false) {
+            inputString.push(inputAux.join(''));
+        }
+        inputString.push(buttonSelection.textContent);
+        inputAux = [];
+    }
+
+    if(buttonSelection.className == 'clear') {
+        inputString = [];
+        inputAux = [];
+        displayString = [];
+        display(displayString);
+    }
+
+    if(inputString.length >= 3 || buttonSelection.textContent == '=') {
+        operationNumber1 = parseInt(inputString[0], 10);
+        operationNumber2 = parseInt(inputString[2], 10);
+        operationOperator = inputString[1];
+        inputString[0] = operate(operationOperator, operationNumber1, operationNumber2);
+        display(inputString[0]);
+        if(buttonSelection.textContent != '=') {
+            inputString[1] = inputString [3];
+            inputString.splice(2,2);
+        }
+        if(buttonSelection.textContent == '=') {
+            inputString.splice(1,3);
+            inputAux = [];
+            inputAux[0] = inputString[0];
+            equalOperation = true;
+        }
+    }
+
+    console.log(inputString);
+    console.log(inputAux);
+    console.log(equalOperation);
+    
 }) 
 
